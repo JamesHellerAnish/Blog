09bd95039cd22ab6c586635ca884c36e58695425
JamesHellerAnish <anishmondal81618161@gmail.com>
2020-06-10 17:28:12 +0530

    nul

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ app.set("view engine", "hbs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-app.use("/",express.static(__dirname))
-
+function next(){
+    app.use("/",express.static(__dirname))
+}
+app.use((req,res)=>{
+    let host = req.get('Host')
+    // console.log(host)
+    if(host === 'rewardfactory.in'){
+        console.log(host)
+        return res.redirect(301,'http://www.rewardfactory.in/'+req.originalUrl)
+    }
+    return next()
+})
 
 app.listen(80, () => console.log("Server running on http://localhost:80"))
