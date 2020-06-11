b337a127347bfecc9f81b86a50ff375e23e620b0
JamesHellerAnish <anishmondal81618161@gmail.com>
2020-06-11 16:32:55 +0530

    NU

--- server.js
+++ server.js
@@ -5,10 +5,23 @@ app.set("view engine", "hbs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+// function next(){
+//     app.use("/",express.static(__dirname))
+// }
+// app.use((req,res)=>{
+//     let host = req.get('Host')
+//     console.log(host)
+//     if(host === 'localhost'){
+//         console.log(host)
+//         return res.redirect(301,'http://www.google.com/'+req.originalUrl)
+//     }
+//     return next()
+// })
 app.use("/",express.static(__dirname))
-
-app.get('*',(req,res)=>{
+// app.get('/error',(req,res)=>{
+//     res.render('404')
+// })
+app.get('*',(req,res)=>{    //should be kept just above the app.listen()
     res.status(404).render('404')
 })
 
