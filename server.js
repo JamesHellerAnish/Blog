1aa5cbf098ec9c39f0291752b86eef7c5c7241cd
JamesHellerAnish <anishmondal81618161@gmail.com>
2020-07-01 16:24:18 +0530

    Revert "Removed"
    This reverts commit dd3cc8b718bd8f8f3cf5931f0ed786a8939115fe.

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const { runInNewContext } = require('vm')
+const app = express()
+
+app.set("view engine", "hbs")
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+// function next(){
+//     app.use("/",express.static(__dirname))
+// }
+// app.use((req,res)=>{
+//     let host = req.header("host")
+//     console.log(host)
+//     // if(host === 'localhost'){
+//     //     console.log(host)
+//     //     return res.redirect(301,'http://www.google.com/'+req.originalUrl)
+//     // }
+//     console.log(host.split('lo')[0])
+//     console.log(host.split('lo')[1])
+//     return next()
+// })
+
+// app.get('/error',(req,res)=>{
+//     res.render('404')
+// })
+app.use('/',(req,res,next)=>{
+    // console.log(req.originalUrl)
+    console.log(req.headers.referer)
+    let host = req.header("host")
+    if(host.split('factory')[0]=='reward'){
+        res.redirect(301,'http://www.rewardfactory.in'+req.originalUrl)
+    }
+    else{
+        next()
+    }
+    
+})
+app.use("/",express.static(__dirname))
+// app.get('*',(req,res)=>{    //should be kept just above the app.listen()
+//     let host = req.header("host")
+//     console.log(host.split('local')[1])
+//     // if(host.split('local')[1]=='host'){
+//     //     res.redirect(301,'http://rewardfactory.in')
+//     // }
+
+//     app.use("/",express.static(__dirname))
+    
+// })
+
+app.get('*',(req,res)=>{
+    // console.log('Hello')
+    res.status(404).render('404')
+})
+
+app.listen(80, () => console.log("Server running on http://localhost:80"))
