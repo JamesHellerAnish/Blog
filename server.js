caa114bccac1219f292ac7f6fce08477858b03d4
JamesHellerAnish <anishmondal81618161@gmail.com>
2020-07-04 23:17:49 +0530

    Changes

--- server.js
+++ server.js
@@ -9,32 +9,13 @@ app.set("view engine", "hbs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-// function next(){
-//     app.use("/",express.static(__dirname))
-// }
-// app.use((req,res)=>{
-//     let host = req.header("host")
-//     console.log(host)
-//     // if(host === 'localhost'){
-//     //     console.log(host)
-//     //     return res.redirect(301,'http://www.google.com/'+req.originalUrl)
-//     // }
-//     console.log(host.split('lo')[0])
-//     console.log(host.split('lo')[1])
-//     return next()
-// })
-
-// app.get('/error',(req,res)=>{
-//     res.render('404')
-// })
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 app.use('/',(req,res,next)=>{
     // console.log(req.originalUrl)
-    console.log(req.headers.referer)
+    // console.log(req.headers.referer)
     let host = req.header("host")
-    if(host.split('factory')[0]=='reward'){
-        res.redirect(301,'http://www.rewardfactory.in'+req.originalUrl)
+    if(host.split('reward')[0]=='www.'){
+        res.redirect(301,'https://rewardfactory.in'+req.originalUrl)
     }
     else{
         next()
@@ -42,25 +23,17 @@ app.use('/',(req,res,next)=>{
     
 })
 app.use("/",express.static(__dirname))
-// app.get('*',(req,res)=>{    //should be kept just above the app.listen()
-//     let host = req.header("host")
-//     console.log(host.split('local')[1])
-//     // if(host.split('local')[1]=='host'){
-//     //     res.redirect(301,'http://rewardfactory.in')
-//     // }
 
-//     app.use("/",express.static(__dirname))
-    
-// })
 const options = {
     key = fs.readFileSync('/etc/letsencrypt/live/rewardfactory.in/privkey.pem','utf8'),
-    cert = fs.readFileSync('/etc/letsencrypt/live/rewardfactory.in/cert.pem','utf8')
+    cert = fs.readFileSync('/etc/letsencrypt/live/rewardfactory.in/cert.pem','utf8'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/rewardfactory.in/chain.pem','utf8')
 }
 app.get('*',(req,res)=>{
-    // console.log('Hello')
     res.status(404).render('404')
 })
 
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(options,app)
-httpsServer.listen(80, () => console.log("Server running on http://localhost:80"))
+httpServer.listen(80, () => console.log("Server running on http://localhost:80"))
+httpsServer.listen(443, () => console.log("Server running on http://localhost:80"))
